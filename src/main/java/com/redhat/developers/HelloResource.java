9f171b82bc1ab07008186d72a5d9fd876c4728bf
Raul Pereira <raul.pereira@gmail.com>
2020-07-27 11:04:51 -0300

    configuration

--- src/main/java/com/redhat/developers/HelloResource.java
+++ src/main/java/com/redhat/developers/HelloResource.java
@@ -1,16 +1,33 @@
 package com.redhat.developers;
 
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 @Path("/hello")
 public class HelloResource {
+
+    @ConfigProperty(name = "greeting")
+    Optional<String> greeting;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        return greeting.orElse("Aloha");
     }
+
+    @Path("/async")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public CompletionStage<String> helloAsync() {
+        return CompletableFuture.supplyAsync(() -> "Hello, I'm async!");
+    }
+
 }
